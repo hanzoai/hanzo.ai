aaad278b11b4cb21857d8e6f10951d6196f29b84
Hanzo Dev <dev@hanzo.ai>
2025-03-06 18:49:21 +0000

    Update Hero component
    Adds a rocket emoji to the end of the "Dream" text in the Hero component.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -50,7 +50,7 @@ const Hero = () => {
   };
 
   const titleText1 = "Build as fast";
-  const titleText2 = "as you Dream";
+  const titleText2 = "as you Dream 🚀";
   const titleLetters1 = titleText1.split("");
   const titleLetters2 = titleText2.split("");
   
