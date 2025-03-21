b74dc8afedf5db399526948f72232422675d5abf
Hanzo Dev <dev@hanzo.ai>
2025-03-21 03:33:25 +0000

    Fix Datastore page loading issue
    Addresses a problem where the Datastore page was failing to load.

--- src/pages/Datastore.tsx
+++ src/pages/Datastore.tsx
@@ -2,11 +2,11 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import HeroSection from "./datastore/HeroSection";
-import KeyFeatures from "./datastore/KeyFeatures";
-import EfficiencySection from "./datastore/EfficiencySection";
-import UseCasesSection from "./datastore/UseCasesSection";
-import GetStartedSection from "./datastore/GetStartedSection";
+import HeroSection from "@/pages/datastore/HeroSection";
+import KeyFeatures from "@/pages/datastore/KeyFeatures";
+import EfficiencySection from "@/pages/datastore/EfficiencySection";
+import UseCasesSection from "@/pages/datastore/UseCasesSection";
+import GetStartedSection from "@/pages/datastore/GetStartedSection";
 
 const Datastore = () => {
   return (
