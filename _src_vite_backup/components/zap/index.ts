b2b202ad64aeb1efa1205588485b7d4ee727771a
Hanzo Dev <dev@hanzo.ai>
2026-02-23 00:18:35 -0800

    chore: remove Vite backup and stale build artifacts

--- _src_vite_backup/components/zap/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ZapHero } from "./ZapHero";
-export { default as ZapFeatures } from "./ZapFeatures";
-export { default as ZapCodeExample } from "./ZapCodeExample";
-export { default as ZapArchitecture } from "./ZapArchitecture";
-export { default as ZapCTA } from "./ZapCTA";
